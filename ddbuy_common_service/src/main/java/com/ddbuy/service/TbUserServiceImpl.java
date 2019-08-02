8a6b9e5aff79105fc928270d6f5e4049372c37ba
lanhai131468 <89932329@qq.com>
2019-08-02 10:46:31 +0800

    sso

--- ddbuy_common_service/src/main/java/com/ddbuy/service/TbUserServiceImpl.java
+++ ddbuy_common_service/src/main/java/com/ddbuy/service/TbUserServiceImpl.java
@@ -6,8 +6,11 @@ import com.ddbuy.inter.TbUserService;
 import com.ddbuy.mapper.TbUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class TbUserServiceImpl implements TbUserService {
  //注入Dao
@@ -19,6 +22,7 @@ public class TbUserServiceImpl implements TbUserService {
  //查询用户名和密码，存在：返回Token 不存在返回：null
  @Override
  public String getToken(String username,String password) {
+  String token=null;
   //创建实体类对象
   TbUserExample userExample = new TbUserExample();
   //获取条件查询的对象
@@ -30,10 +34,12 @@ public class TbUserServiceImpl implements TbUserService {
   List<TbUser> tbUsers = tbUserMapper.selectByExample(userExample);
   //判断用户信息是否存在
   if (tbUsers.size()!=0){
-
+   //使用随机数产生Token
+   token= UUID.randomUUID().toString();
+   //将Token存入到Redis中
+   ValueOperations<String,Object> operations = redisTemplate.opsForValue();
+   operations.set("sessionkey:"+token,tbUsers.get(0),20, TimeUnit.MINUTES);
   }
-
-
-  return null;
+  return token;
  }
 }
---
+++ ddbuy_sso_web/src/main/java/com/ddbuy/DdbuySsoWebApplication.java
@@ -0,0 +1,15 @@
+package com.ddbuy;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDubboConfiguration  //启动dubbo 发布相关配置
+public class DdbuySsoWebApplication {
+
+ public static void main(String[] args) {
+  SpringApplication.run(DdbuySsoWebApplication.class, args);
+ }
+
+}
